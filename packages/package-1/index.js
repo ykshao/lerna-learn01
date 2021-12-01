d130c2327c9a8cf7b2de0461db981322ab86e052
yongkai.shao <yongkai.shao@vhall.com>
2021-12-01 17:15:48 +0800

    测试

--- packages/package-1/index.js
+++ packages/package-1/index.js
@@ -2,6 +2,7 @@ const base = require('@fork-fe/base')
 const _ = require('lodash')
 base.test()
 console.log(_.VERSION);
+console.log("111111")
 
 
 // https://static.vhallyun.com/??jssdk/vhall-jssdk-base/vhall-msg-1.0.9.js,jssdk/vhall-jssdk-chat/latest/vhall-jssdk-chat-2.0.9.js,jssdk/vhall-jssdk-doc/latest/vhall-jssdk-doc-3.1.6.js
